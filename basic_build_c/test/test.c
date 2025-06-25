621e7bef44a8f3db5278dc11d445e4469ceb2f2f
Seth Stephanz <sethstephanz@gmail.com>
2025-06-25 14:27:05 -0400

    Clean out contents from template

--- basic_build_c/test/test.c
+++ basic_build_c/test/test.c
@@ -1,65 +1,6 @@
 #include <stdio.h>
 
-// process management
-// fork()
-// waitpid()
-// wait()
-// execve()
-// exit()
-// brk()
-// getpid()
-// getgrp()
-// setsid()
-// ptrace()
+int my_tests() {
+    return 0;
+}
 
-// signals
-// sigaction()
-// sigreturn()
-// sigprocmask()
-// sigpending()
-// sigsuspend()
-// kill()
-// alarm()
-
-// file management
-// creat()
-// mkdnod()
-// open()
-// close()
-// read()
-// write()
-// lseek()
-// stat()
-// fstt()
-// dup()
-// pipe()
-// ioctl()
-// access()
-// rename()
-// fcntl()
-
-// directory and file system managment
-// mkdir()
-// rmdir()
-// link()
-// unlink()
-// mount()
-// unmount()
-// sync()
-// chdir()
-// chroot()
-
-// protection
-// chmod()
-// getuid()
-// getgid()
-// setuid()
-// setgid()
-// chown()
-// umask()
-
-// time management
-// time()
-// stime()
-// untime()
-// times()
